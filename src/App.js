3a2387863d7232473d29ce6c72908f59004f84fb
Manoj Kumar <mu.kumaran1991@gmail.com>
2025-03-04 16:50:23 +0530

    new addition

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App()
   const [rno,setRno] = useState(0)
   const [sname,setSname] = useState("")
   const [mark,setMark] = useState(0)
+  const [list,setList] = useState(null)
 
   const dis = (e)=>{
     if(e.target.name === "rno")
@@ -25,9 +26,18 @@ function App()
 
     axios.get("http://127.0.0.1:1234",{params:inputdata}).then((res)=>{
         console.log(res)
+        console.log(res.data)
+        setList(res.data)
     }).catch((err)=>{
       console.log(err.response.data)
+      setList(err.response.data)
     })
+    cleartextbox()
+  }
+  const cleartextbox = ()=>{
+    setRno("")
+    setSname("")
+    setMark("")
   }
 
   return(
@@ -35,8 +45,10 @@ function App()
     <h2>Student details</h2>
     Roll no: <input type="text" placeholder="Enter roll no" name="rno" value={rno} onChange={(e)=>dis(e)}></input><br></br>
     Student name : <input type="text" placeholder="Enter student name" name="sname" value={sname} onChange={(e)=>dis(e)}></input><br></br>
-    mark: <input type="text" placeholder="Enter mark" name="mark" value={mark} onChange={(e)=>dis(e)}></input><br></br>
+    Mark: <input type="text" placeholder="Enter mark" name="mark" value={mark} onChange={(e)=>dis(e)}></input><br></br>
     <input type="button" value={"Insert/Save"} onClick={insertfun}></input>
+    <h1>{list !== null && list.acknowledged === true && "Successfully Inserted"}</h1>
+    <h1>{list !== null && list.acknowledged === true && "ID:"+list.insertedId}</h1>
     </>
   )
 }
